306423c974f1662393ecbc795cd96f6b82a7a2fc
Osman <osmanpitufo112233@gmail.com>
2021-02-28 23:50:17 -0600

    Se realizo la calculadora en c#

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Calculadora_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cantidadA;
+            double cantidadB;
+            string operacionX;
+            double resultado;
+
+            Console.WriteLine("Escriba la operacion que quiere realizar");
+            Console.WriteLine("1.SUMA");
+            Console.WriteLine("2.RESTA");
+            Console.WriteLine("3.Multiplicacion");
+            Console.WriteLine("4.Division");
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("Ingrese el numero de la operacion ");
+
+            operacionX = Console.ReadLine();
+
+            Console.WriteLine("Escriba la primer cantidad");
+            cantidadA = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("************************************************");
+            Console.WriteLine("Escriba la segunda cantidad");
+            cantidadB = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("************************************************");
+            Console.Clear();
+            
+            
+
+            if(operacionX=="1")
+            {
+                resultado= cantidadA + cantidadB;
+                Console.WriteLine("Tu respuesta es:"+ resultado);
+                Console.ReadLine();
+            }
+            if(operacionX=="2")
+            {
+                resultado= cantidadA - cantidadB;
+                Console.WriteLine("Tu respuesta es:"+ resultado);
+                Console.ReadLine();
+            }
+            if(operacionX=="3")
+            {
+                resultado= cantidadA * cantidadB;
+                Console.WriteLine("Tu respuesta es:"+ resultado);
+                Console.ReadLine();
+            }
+            if(operacionX=="4")
+            {
+                resultado= cantidadA / cantidadB;
+                Console.WriteLine("Tu respuesta es:"+ resultado);
+                Console.ReadLine();
+            }
+        }
+    }
+}
